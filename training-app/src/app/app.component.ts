35989af6564409a3387e9b02502d91f778202c5d
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2022-04-17 12:42:45 +0300

    Toastr_warning messages

--- training-app/src/app/app.component.ts
+++ training-app/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//ngx Toast
+import { Toast, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'training-app';
+  constructor(private toastr: ToastrService) {}
+
+
+  ngOnInit(): void {
+    this.showSuccess();
+  }
+  showSuccess() {
+    this.toastr.success('Hello world!', 'Toastr fun!');
+  }
+
 }
--- training-app/src/app/app.module.ts
+++ training-app/src/app/app.module.ts
@@ -4,13 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+// ngx toastr
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    //ng Toastr
+    CommonModule ,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      //Options
+      timeOut:4000,
+      progressBar:true,
+      easing:"ease-in",
+      closeButton:true,
+      progressAnimation:"increasing",
+      preventDuplicates:true
+    }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
