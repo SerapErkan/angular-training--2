dbc2ef0e0a01d26f6457f1be228228530840c42a
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2022-04-18 17:48:17 +0300

    Ignite-UI for Angular

--- training-app/src/app/app.module.ts
+++ training-app/src/app/app.module.ts
@@ -1,12 +1,11 @@
 // Spinner
-import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NgxSpinnerModule } from "ngx-spinner";
 
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 
 // ngx toastr
 import { CommonModule } from '@angular/common';
@@ -22,24 +21,24 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule,
 
     //ng Toastr
-    CommonModule ,
+    CommonModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       //Options
-      timeOut:4000,
-      progressBar:true,
-      easing:"ease-in",
-      closeButton:true,
-      progressAnimation:"increasing",
-      preventDuplicates:true
+      timeOut: 4000,
+      progressBar: true,
+      easing: "ease-in",
+      closeButton: true,
+      progressAnimation: "increasing",
+      preventDuplicates: true
     }),
 
-    NgxSpinnerModule, //Spinner
-    
+    NgxSpinnerModule
   ],
 
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA], //Spinner
 })
-export class AppModule { }
+export class AppModule {
+}
