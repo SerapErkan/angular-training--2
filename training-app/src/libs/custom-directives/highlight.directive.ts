efd9b6b67b769da7175f1e5f5cb8b431249c9a0a
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2022-04-21 22:40:45 +0300

    Custom Directive-Input()

--- training-app/src/libs/custom-directives/highlight.directive.ts
+++ training-app/src/libs/custom-directives/highlight.directive.ts
@@ -1,14 +1,26 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
+  @Input() highlightColor :string= '';
+
   constructor(private el:ElementRef) 
   { 
- el.nativeElement.style.backgroundColor="orange"; // arkaplan rengini değiştir
+  // el.nativeElement.style.backgroundColor="orange"; 
   }
+
+ @HostListener('mouseenter') onMouseEnter() {
+    this.el.nativeElement.style.backgroundColor=this.highlightColor;
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.el.nativeElement.style.backgroundColor=this.highlightColor;
+  }
+
+  // 
 
   // @HostListener('mouseenter') onMouseEnter() {
   //   this.highlight('blue');
@@ -21,6 +33,8 @@ export class HighlightDirective {
   // private highlight(color: string) {
   //   this.el.nativeElement.style.backgroundColor = color;
   // }
+
+  // makeInvisible()***************
 
   // @HostListener('mouseenter') onMouseEnter() {
   //   this.makeInvisible();
@@ -39,23 +53,30 @@ export class HighlightDirective {
   // makeVisible(){
   //   this.el.nativeElement.style="visibility:visible";
   // }
+
+
+  //makeBigger()**********
   
-  @HostListener('mouseenter') onMouseEnter() {
-    this.makeBigger();
-  }
+  // @HostListener('mouseenter') onMouseEnter() {
+  //   this.makeBigger();
+  // }
 
-  @HostListener('mouseleave') onMouseLeave() {
-    this.makeSmaller();
-  }
+  // @HostListener('mouseleave') onMouseLeave() {
+  //   this.makeSmaller();
+  // }
 
-  private highlight(color: string) {
-    this.el.nativeElement.style.backgroundColor = color;
-  }
-  makeBigger(){
-    this.el.nativeElement.style="font-size:2rem";
-  }
-  makeSmaller(){
-    this.el.nativeElement.style="fony-size:0.8rem"
-  }
+  // private highlight(color: string) {
+  //   this.el.nativeElement.style.backgroundColor = color;
+  // }
+  // makeBigger(){
+  //   this.el.nativeElement.style="font-size:2rem";
+  // }
+  // makeSmaller(){
+  //   this.el.nativeElement.style="fony-size:0.8rem"
+  // }
+
+
+
+
 
 }
